2cf53e6acb0df265aa6c6f7b65ce36857cd95ebf
DevSahuf <142213016+DevSahuf@users.noreply.github.com>
2024-02-16 12:46:13 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Basic widget"),
+          backgroundColor: Colors.purple,
+          
+          ),
+           drawer: Drawer(
+            child: ListView(children: <Widget>[
+              DrawerHeader(
+                child: Text('Sahuf')),
+              ListTile(
+                title: Text('HOME'),
+                onTap: ()
+                {},
+              ),
+              ListTile(
+                title: Text('ABOUT US'),
+                onTap: ()
+                {},
+              ),
+              ListTile(
+                title: Text('CONTACT US'),
+                onTap: ()
+                {},
+              ),
+            ],),
+           ),
+           body:Center(
+            child:Column(
+              children: [
+                Image.asset('assets/images/404.png',
+                 width: 200),
+                 SizedBox(height: 20),
+                 Text("404 not found"),],),
+              
+           ),
+           bottomNavigationBar: BottomNavigationBar(items:[
+BottomNavigationBarItem(icon: Icon(Icons.home),label: 'home'),
+BottomNavigationBarItem(icon: Icon(Icons.settings),label: 'settings'),
+BottomNavigationBarItem(icon: Icon(Icons.search),label: 'search'),
+           ],
+           selectedItemColor: Colors.blue,
+           onTap:(int index){},
+           ),
+          
+          ),
+    );
+  }
+}
